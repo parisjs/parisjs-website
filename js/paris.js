92d7f76e0402222b5c8c1fba04795367a408d748
François de Metz <francois@2metz.fr>
2011-08-24 22:07:49 +0200

    Reindent.

--- js/paris.js
+++ js/paris.js
@@ -36,7 +36,8 @@ var Nav = {
         function processScroll(e) {
             var scrollTop = $window.scrollTop() + 10, i;
             for (i = offsets.length; i--;) {
-                if (activeTarget != targets[i] && scrollTop >= offsets[i] && (!offsets[i + 1] || scrollTop <= offsets[i + 1])) {                    activeTarget = targets[i];
+                if (activeTarget != targets[i] && scrollTop >= offsets[i] && (!offsets[i + 1] || scrollTop <= offsets[i + 1])) {
+                    activeTarget = targets[i];
                     setButton(activeTarget);
                 }
             }
