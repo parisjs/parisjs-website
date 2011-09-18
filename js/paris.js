2360fdf23b3e0bd9481c21ee24e260500f0214d5
François de Metz <francois@2metz.fr>
2011-09-18 15:41:46 +0200

    Upgrade to bootsrap 1.3.
    Update the header, add the new bootstrap-scrollspy.js jquery plugin.
    
    Signed-off-by: François de Metz <francois@2metz.fr>

--- js/paris.js
+++ js/paris.js
@@ -15,44 +15,6 @@ var ParisJS = {
     ICAL: "http://h2vx.com/ics/www.eventbrite.com/org/862067525"
 }
 
-var Nav = {
-    init: function() {
-        var activeTarget,
-        $window = $(window),
-        nav = $('body .topbar li a'),
-        targets = nav.map(function () {
-            return $(this).attr('href');
-        }),
-        offsets = $.map(targets, function (id) {
-            return $(id).offset().top;
-        });
-
-
-        function setButton(id) {
-            nav.parent("li").removeClass('active');
-            $(nav[$.inArray(id, targets)]).parent("li").addClass('active');
-        }
-
-        function processScroll(e) {
-            var scrollTop = $window.scrollTop() + 10, i;
-            for (i = offsets.length; i--;) {
-                if (activeTarget != targets[i] && scrollTop >= offsets[i] && (!offsets[i + 1] || scrollTop <= offsets[i + 1])) {
-                    activeTarget = targets[i];
-                    setButton(activeTarget);
-                }
-            }
-        }
-
-        nav.click(function () {
-            processScroll();
-        });
-
-        processScroll();
-
-        $window.scroll(processScroll);
-    }
-}
-
 var Tabs = {
     init: function() {
         function getPanel(tab) {
@@ -156,7 +118,7 @@ window.ParisJS = ParisJS;
 $(function() {
     Meetups.init();
     Tabs.init();
-    Nav.init();
+    $('.topbar').scrollSpy()
 });
 
 })(jQuery);
