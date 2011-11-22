e2c5dee6daadcf3345eeeb555d5c78d14c00c110
David Thevenin <david.thevenin@gmail.com>
2011-11-22 22:55:31 +0100

    Enhance js and css code
    - fix link on eventbrite
    - enhance view generation
    - css remove duplicate code

--- js/google_spreadsheet.js
+++ js/google_spreadsheet.js
@@ -1,22 +1,24 @@
-var Spin = { };
+var Spin = {};
 
-Spin.init = function (target) {
-    var opts = {
-      lines: 16,
-      length: 9,
-      width: 2,
-      radius: 12,
-      color: '#DC4084',
-      speed: 1,
-      trail: 100,
-      shadow: true
-    };
+Spin.init = function (target)
+{
+  var opts = {
+    lines: 16,
+    length: 9,
+    width: 2,
+    radius: 12,
+    color: '#DC4084',
+    speed: 1,
+    trail: 100,
+    shadow: true
+  };
 
-    this.spinner = new Spinner(opts).spin(target);
+  this.spinner = new Spinner(opts).spin(target);
 };
 
-Spin.stop = function () {
-    this.spinner.stop();
+Spin.stop = function ()
+{
+  this.spinner.stop();
 };
 
 /****************************************************************
@@ -36,12 +38,27 @@ function Talk (title, authors, contacts, type, abstract, state)
   this.state = state;
 }
 
+Talk.TEMPLATE = "<article class=\"prez\"><header><h1>{title}</h1></header><section class=\"authors\"><span>{authors}</span><span>{contacts}</span></section><section class=\"abstract\"><pre>{abstract}</pre></section></article>";
+
 Talk.TYPE_NORMAL = 1;
 Talk.TYPE_SHORT = 2;
 
-Talk.STATE_ACCEPTED = 0;
-Talk.STATE_NEXT = 1;
-Talk.STATE_NEXT_NEXT = 2;
+Talk.STATE_ACCEPTED = 'Accepte';
+Talk.STATE_NEXT = 'Prochain';
+Talk.STATE_NEXT_NEXT = 'Suivant';
+
+Talk.prototype.generateView = function ()
+{
+  var str = Talk.TEMPLATE;
+  for (var key in this) str = str.replace ('{' + key + '}', this [key]);
+
+  var div = document.createElement (div);
+  div.innerHTML = str;
+  div = div.firstElementChild;
+    
+  if (this.type === Talk.TYPE_SHORT) div.className = "prez lightning";
+  return div;
+}
 
 Talk.hasAcceptedTalk = false;
 
@@ -56,12 +73,9 @@ function parseRow (row)
   var type =
     (row[4] && row[4].v.indexOf ("Lightning") === 0)?Talk.TYPE_SHORT:Talk.TYPE_NORMAL;
   var abstract = (row[5])?row[5].v:"";
-  var state = (row[7])?row[7].v:"Suivant";
-  if (state == "Accepte") state = Talk.STATE_ACCEPTED ;
-  else if (state == "Prochain") state = Talk.STATE_NEXT;
-  else if (state == "Suivant") state = Talk.STATE_NEXT_NEXT;
+  var state = (row[7])?row[7].v:Talk.STATE_NEXT_NEXT;
   
-  if (state === 0) Talk.hasAcceptedTalk = true;
+  if (state == Talk.STATE_ACCEPTED) Talk.hasAcceptedTalk = true;
   
   return new Talk (title, authors, contacts, type, abstract, state);
 }
@@ -100,39 +114,10 @@ function generateGUI (className, talks, state)
     var talk = talks[i];
     if (talk.state != state) continue;
     
-    var article = document.createElement ('article');
+    article = talk.generateView ();
     
-    if (talk.type === Talk.TYPE_SHORT)
-    {
-      article.className = "prez lightning";
-      div_lightning.appendChild (article);
-    }
-    else
-    {
-      article.className = "prez";
-      div_normal.appendChild (article);
-    }
-
-    var header = document.createElement ('header');
-    header.innerHTML = "<h1>" + talk.title + "</h1>";
-    article.appendChild (header);
-
-    section = document.createElement ('section');
-    article.appendChild (section);
-    section.className = "authors";
-
-    var span = document.createElement ('span');
-    section.appendChild (span);
-    span.innerHTML = talk.authors;
-
-    span = document.createElement ('span');
-    section.appendChild (span);
-    span.innerHTML = talk.contacts;
-
-    section = document.createElement ('section');
-    article.appendChild (section);
-    section.className = "abstract";
-    section.innerHTML = "<pre>" + talk.abstract + "</pre>";
+    if (talk.type === Talk.TYPE_SHORT) div_lightning.appendChild (article);
+    else div_normal.appendChild (article);
   }
 }
 
@@ -158,7 +143,7 @@ function queryResult (response)
   document.getElementById ("spin").style.display = "none";
   document.getElementById ("content").style.visibility = "visible";
   
-  if (response.isError())
+  if (response.isError ())
   {
     alert ('Error in query: ' + response.getMessage() + ' ' + 
       response.getDetailedMessage());
@@ -166,19 +151,14 @@ function queryResult (response)
   }
   var data = response.getDataTable ();
   var talks = parseDataTable (data);
+  
   if (Talk.hasAcceptedTalk)
-  {
-    generateGUI (".prez", talks, 0);
-  }
+    generateGUI (".prez", talks, Talk.STATE_ACCEPTED);
   else
-  {
-    var prez = document.querySelector (".prez");
-    var title = document.createElement ('h2');
-    title.innerHTML = "Programme du prochain ParisJS en cours de finalisation...";
-    prez.appendChild (title);
-  }
-  generateGUI (".prez.prochain", talks, 1);
-  generateGUI (".prez.avenir", talks, 2);
+    document.querySelector (".prez").innerHTML = "<h2>Programme du prochain ParisJS en cours de finalisation...<h2>";
+
+  generateGUI (".prez.prochain", talks, Talk.STATE_NEXT);
+  generateGUI (".prez.avenir", talks, Talk.STATE_NEXT_NEXT);
 }
 
 /**
@@ -186,19 +166,7 @@ function queryResult (response)
  */
 function requestData ()
 {
-  var query = new google.visualization.Query (    'http://spreadsheets.google.com/tq?key=0AnLhUwtBNx3zdDNDbU93eUh1NXpCenNXT1FqQksxZmc&pub=1');
+  var query = new google.visualization.Query (  'http://spreadsheets.google.com/tq?key=0AnLhUwtBNx3zdDNDbU93eUh1NXpCenNXT1FqQksxZmc&pub=1');
 
   query.send (queryResult)
-}
-
-/****************************************************************
- *
- ****************************************************************/
-
-/**
- *
- */
-function init ()
-{
-  Spin.init (document.getElementById ("spin"));
 }
