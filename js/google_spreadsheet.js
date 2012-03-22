74e5825a028fa97bdad061284e2c983fd3b48806
François de Metz <francois@2metz.fr>
2012-03-22 22:58:59 +0100

    Rewrite the program.html code using backbone for fun and profit.
    The best way to understand code it to rewrite it. Done.
    
    Signed-off-by: François de Metz <francois@stormz.me>

--- js/google_spreadsheet.js
+++ js/google_spreadsheet.js
@@ -1,7 +1,6 @@
 var Spin = {};
 
-Spin.init = function (target)
-{
+Spin.init = function(target) {
   var opts = {
     lines: 16,
     length: 9,
@@ -16,171 +15,208 @@ Spin.init = function (target)
   this.spinner = new Spinner(opts).spin(target);
 };
 
-Spin.stop = function ()
-{
+Spin.stop = function() {
   this.spinner.stop();
 };
- onload="Spin.init (document.getElementById ('spin'));"
-/****************************************************************
- *    Data and Parse
- ****************************************************************/
+
 
 /**
- *
+ * Backbone App
  */
-function Talk (title, authors, contacts, type, abstract, state)
-{
-  this.title = title;
-  this.authors = authors;
-  this.contacts = contacts;
-  this.type = type;
-  this.abstract = abstract;
-  this.state = state;
-}
-
-Talk.TEMPLATE = "<article class=\"prez\"><header><h1>{title}</h1></header><section class=\"authors\"><span>{authors}</span><span>{contacts}</span></section><section class=\"abstract\"><p>{abstract}</p></section></article>";
-
-Talk.TYPE_NORMAL = 1;
-Talk.TYPE_SHORT = 2;
-
-Talk.STATE_ACCEPTED = 'Accepte';
-Talk.STATE_NEXT = 'Prochain';
-Talk.STATE_NEXT_NEXT = 'Suivant';
-
-Talk.prototype.generateView = function ()
-{
-  var str = Talk.TEMPLATE;
-  for (var key in this)
-  {
-    text = this [key];
-    if (text.replace) text = text.replace (/\n/g, '<br/>');
-    str = str.replace ('{' + key + '}',text);
-  }
-
-  var div = document.createElement (div);
-  div.innerHTML = str;
-  div = div.firstElementChild;
-    
-  if (this.type === Talk.TYPE_SHORT) div.className = "prez lightning";
-  return div;
-}
-
-Talk.hasAcceptedTalk = false;
+var App = {
+    models: {},
+    views: {},
+    collections: {}
+};
 
 /**
- *
+ * Tall model
  */
-function parseRow (row)
-{
-  var title = (row[1])?row[1].v:"";
-  var authors = (row[2])?row[2].v:"";
-  var contacts = (row[3])?row[3].v:"";
-  var type =
-    (row[4] && row[4].v.indexOf ("Lightning") === 0)?Talk.TYPE_SHORT:Talk.TYPE_NORMAL;
-  var abstract = (row[5])?row[5].v:"";
-  var state = (row[7])?row[7].v:Talk.STATE_NEXT_NEXT;
-  
-  if (state == Talk.STATE_ACCEPTED) Talk.hasAcceptedTalk = true;
-  
-  return new Talk (title, authors, contacts, type, abstract, state);
-}
+App.models.Talk = Backbone.Model.extend({
+    // normal talk (10-20 min)
+    TYPE_NORMAL     : 1,
+    // lightning talk
+    TYPE_SHORT      : 2,
+
+    // accepted scheduled for the next parisjs
+    STATE_ACCEPTED  : 'Accepte',
+    // can be scheduled on the next parisjs by not yet accepted
+    STATE_NEXT      : 'Prochain',
+    // cannot be scheduled on the next parisjs
+    STATE_NEXT_NEXT : 'Suivant',
+
+    defaults: {
+        title: '',
+        authors: '',
+        contacts: '',
+        type: '',
+        abstract: '',
+        state: ''
+    },
+
+    /**
+     * Parse the json returned by the spreadsheet
+     */
+    parse: function(response) {
+        var row = response.c;
+        var title = (row[1]) ?row[1].v : "";
+        var authors = (row[2])  ? row[2].v : "";
+        var contacts = (row[3]) ? row[3].v : "";
+        var type = (row[4] && row[4].v.indexOf("Lightning") === 0) ? this.TYPE_SHORT : this.TYPE_NORMAL;
+        var abstract = (row[5]) ? row[5].v: "";
+        var state = (row[7]) ? row[7].v : this.STATE_NEXT_NEXT;
+        return {
+            title: title,
+            authors: authors,
+            contacts: contacts,
+            type: type,
+            abstract: abstract,
+            state: state
+        };
+    }
+});
 
 /**
- *
+ * Collection of talks
  */
-function parseDataTable (dataTable)
-{
-  var talks = [];
-  
-  // oui c un peu con mais au moins c plus simple a parser
-  var data = JSON.parse (dataTable.toJSON ());
+App.collections.Talks = Backbone.Collection.extend({
+    model: App.models.Talk,
 
-  for (var i = 0; i < data.rows.length; i++)
-  {
-    talks.push (parseRow (data.rows [i].c));
-  }
-  return talks;
-}
+    /**
+     * Return only accepted prezs
+     */
+    accepted: function() {
+        return this.where({state: App.models.Talk.prototype.STATE_ACCEPTED});
+    },
 
-/****************************************************************
- *    Gui generation
- ****************************************************************/
+    /**
+     * Return all non-accepted prezs
+     */
+    submitted: function() {
+        return this.filter(function(talk) {
+            return talk.get('state') != App.models.Talk.prototype.STATE_ACCEPTED;
+        });
+    },
+
+    /**
+     * Parse the json returned by the spreadsheet
+     */
+    parse: function(response) {
+        // the easiest way :(
+        var data = JSON.parse(response.toJSON());
+        return data.rows;
+    },
+
+    /**
+     * Custom sync for google spreadsheets
+     */
+    sync: function(method, model, options) {
+        if (method != "read") throw new Error("not (yet) supported");
+        var query = new google.visualization.Query('http://spreadsheets.google.com/tq?key=0AnLhUwtBNx3zdDNDbU93eUh1NXpCenNXT1FqQksxZmc&pub=1');
+        query.send(function(response) {
+            if (response.isError()) options.error(response);
+            else options.success(response.getDataTable());
+        });
+    }
+});
 
 /**
- *
+ * Render a talk
  */
-function generateGUI (className, talks, state)
-{
-  var div_normal = document.querySelector (className + ' .normal');
-  var div_lightning = document.querySelector (className + ' .lightning');
-  
-  for (var i = 0; i < talks.length; i++)
-  {
-    var talk = talks[i];
-    if (talk.state != state) continue;
-    
-    article = talk.generateView ();
-    
-    if (talk.type === Talk.TYPE_SHORT) div_lightning.appendChild (article);
-    else div_normal.appendChild (article);
-  }
-}
+App.views.Talk = Backbone.View.extend({
+    tagName: 'article',
+    className: 'prez',
 
-/****************************************************************
- * Google Stuff
- ****************************************************************/
+    render: function() {
+        var template = _.template($('#talk-template').html());
+        var data = this.model.toJSON();
+        data.abstract = data.abstract.replace(/\n/g, '<br/>');
+        this.$el.append(template(data));
+
+        if (this.model.get('type') === this.TYPE_SHORT)
+            this.$el.addClass("lightning");
+
+        return this;
+    }
+});
+/**
+ * Render a collection of talks
+ */
+App.views.Talks = Backbone.View.extend({
+    initialize: function() {
+        this.collection.bind('reset', this.render, this);
+    },
+
+    render: function() {
+        this.collection.each(_.bind(this.renderTalk, this));
+        return this;
+    },
+
+    renderTalk: function(talkModel) {
+        var talkView = new App.views.Talk({model: talkModel}).render();
+        this.$el.append(talkView.el);
+    }
+});
 
 /**
- *
+ * The basic router of the page
  */
-function _google_on_laoded ()
-{
-  google.load("visualization", "1");
-  google.setOnLoadCallback (requestData);
-}
+App.Program = Backbone.Router.extend({
+
+    routes: {
+        "": "index",
+        "submitted": "submitted"
+    },
+
+    initialize: function() {
+        Spin.init($('#spin').get(0));
+        this.talks = new App.collections.Talks();
+        this.talksAccepted = new App.collections.Talks();
+        this.talksSubmitted = new App.collections.Talks();
+        this.talks.on('reset', this.onReset, this);
+        this.talks.fetch();
+    },
+
+    onReset: function() {
+        $("#spin").hide();
+        Spin.stop();
+        var accepted = this.talks.accepted();
+        if (accepted.length > 0) {
+            this.talksAccepted.reset(accepted);
+        } else {
+            // show only submitted talks
+            this.submitted();
+            $('.switch').hide();
+        }
+        var submitted = this.talks.submitted();
+        if (submitted.length > 0) {
+            this.talksSubmitted.reset(submitted);
+        }
+        new App.views.Talks({collection: this.talksAccepted,
+                             el: $('#prezs-scheduled .prezs').get(0)}).render();
+
+        new App.views.Talks({collection: this.talksSubmitted,
+                             el: $('#prezs-submitted .prezs').get(0)}).render();
+    },
+
+    index: function() {
+        $('#prezs-scheduled').show();
+        $('#prezs-submitted').hide();
+    },
+
+    submitted: function() {
+        $('#prezs-scheduled').hide();
+        $('#prezs-submitted').show();
+    }
+});
 
 /**
- *
+ * Callback when the google jsapi is loaded
  */
-function queryResult (response)
-{
-  Spin.stop ();
-  document.getElementById ("spin").style.display = "none";
-  document.getElementById ("content").style.visibility = "visible";
-  
-  if (response.isError ())
-  {
-    alert ('Error in query: ' + response.getMessage() + ' ' + 
-      response.getDetailedMessage());
-    return;
-  }
-  var data = response.getDataTable ();
-  var talks = parseDataTable (data);
-  
-  // Un programme est dispo
-  if (Talk.hasAcceptedTalk)
-  {
-    generateGUI (".prez", talks, Talk.STATE_ACCEPTED);
-    document.querySelector (".prez.prochain").style.visibility = "hidden";
-    document.querySelector (".prez.avenir").style.visibility = "hidden";
-  }
-  // Pas de programme dispo, on affiche les propositions
-  else
-  {
-    document.querySelector ("a.add_prez").style.visibility = "visible";
-    document.querySelector (".prez").innerHTML = "<h2>Programme du prochain ParisJS en cours de finalisation...<h2>";
-
-    generateGUI (".prez.prochain", talks, Talk.STATE_NEXT);
-    generateGUI (".prez.avenir", talks, Talk.STATE_NEXT_NEXT);
-  }
-}
-
-/**
- *
- */
-function requestData ()
-{
-  var query = new google.visualization.Query (  'http://spreadsheets.google.com/tq?key=0AnLhUwtBNx3zdDNDbU93eUh1NXpCenNXT1FqQksxZmc&pub=1');
-
-  query.send (queryResult)
+function _google_on_loaded() {
+    google.load("visualization", "1");
+    google.setOnLoadCallback(function() {
+        new App.Program();
+        Backbone.history.start();
+    });
 }
