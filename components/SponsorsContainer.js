8864ae16ff61d3ae594e975a8e7b6ed75dbb65b2
Pierrick <mulotmail@gmail.com>
2017-11-06 17:23:38 +0100

    Add basic sponsors page

--- components/SponsorsContainer.js
+++ components/SponsorsContainer.js
@@ -2,37 +2,39 @@ import React from 'react'
 import Head from 'react-helmet'
 import { createContainer, query, BodyRenderer } from '@phenomic/preset-react-app/lib/client'
 import { Link } from 'react-router'
+import { FormattedMessage, FormattedHTMLMessage } from 'react-intl'
 
 import Layout from './Layout'
 import MeetupPreview from './MeetupPreview'
 
-const SponsorsContainer = () => (
-  <Layout>
-    <Head>
-      <title>Hello world</title>
-      <meta name="description" content="Everything is awaysome!" />
-    </Head>
+const SponsorsContainer = () => {
 
-    <form>
-      <label>
-        Entité (nom de la personne, école, entreprise, etc...) :
-        <input type="text" name="who" required />
-      </label>
+  const sponsors = [
+    { name: 'Parisjs', img: '/sponsors/parisjs.png' }
+  ]
 
-      <label>
-        Type
-        <input type="checkbox" name="type" value="host" /> Lieu<br />
-        <input type="checkbox" name="type" value="food" /> Buffet
-      </label>
+  return (
+    <Layout>
+      <Head>
+        <title>Hello world</title>
+        <meta name="description" content="Everything is awaysome!" />
+      </Head>
 
-      <label>
-        Nombre de places
-        <input type="number" name="hostSize" required />
-      </label>
+      <h1>
+        <FormattedMessage id="SPONSOR_TITLE" />
+      </h1>
+      <p>
+        <FormattedHTMLMessage id="SPONSOR_INTRO" />
+      </p>
 
-      <input type="submit" value="Submit" />
-    </form>
-  </Layout>
-)
+      { sponsors.map(({ img, name }) => (
+        <figure key={ name }>
+          <img src={ img } alt={ name } />
+          <figcaption>{ name }</figcaption>
+        </figure>
+      )) }
+    </Layout>
+  )
+}
 
 export default SponsorsContainer
--- intl.js
+++ intl.js
@@ -15,7 +15,10 @@ const messages = {
     PAGE_SPONSORS: '/en/sponsors',
     PAGE_SUBMIT_TALK: '/en/submission/talk',
     PAGE_FAQ: '/en/faq',
-    FAQ: 'FAQ'
+    FAQ: 'FAQ',
+    SPONSOR_TITLE: 'Our sponsors',
+    SPONSOR_DESCRIPTION: 'List of our latest sponsors. Sponsorise Paris.js on submit this form.',
+    SPONSOR_INTRO: 'A big thank you to the sponsoring companies for hosting us on their offices or offering the buffet.<br />If you want to sponsor us, just fill in <a href="https://docs.google.com/forms/d/e/1FAIpQLSfyJchaXT8Rv1bMFtdECCeW5Np-TvN1QVipYQvcfMLdLj6Vcw/viewform">this form</a>'
   },
   fr: {
     HOME: 'Accueil',
@@ -25,7 +28,11 @@ const messages = {
     PAGE_SPONSORS: '/partenaires',
     PAGE_SUBMIT_TALK: '/propositions/sujet',
     PAGE_FAQ: '/faq',
-    FAQ: 'FAQ'
+    FAQ: 'FAQ',
+    SPONSOR_TITLE: 'Nos partenaires',
+    SPONSOR_DESCRIPTION: 'Liste de nos derniers partenaires. Sponsorisez un Paris.js en replissant le formulaire.',
+    SPONSOR_INTRO: 'Un grand merci aux entreprises sponsors de nous avoir accueilli dans leurs locaux ou offert le buffet.<br />Si vous souhaitez nous sponsoriser, il suffit de remplir <a href="https://docs.google.com/forms/d/e/1FAIpQLSfyJchaXT8Rv1bMFtdECCeW5Np-TvN1QVipYQvcfMLdLj6Vcw/viewform">ce formulaire</a>'
+
   }
 }
 
@@ -51,10 +58,10 @@ export function getIntl(locale) {
 
 export function IntlLink({ to, title }) {
   return (
-    <FormattedMessage id={ to }>
+    <FormattedMessage id={to}>
       {(txt) => (
-        <Link to={ txt }>
-          <FormattedMessage id={ title } />
+        <Link to={txt}>
+          <FormattedMessage id={title} />
         </Link>
       )}
     </FormattedMessage>
@@ -65,8 +72,8 @@ export function LocalLink({ to, children }) {
   const locale = getLocale(typeof location !== 'undefined' ? location.pathname : '/')
 
   return (
-    <Link to={ `${locale !== defaultLocale ? locale : ''}${to}` } >
-      { children }
+    <Link to={`${locale !== defaultLocale ? locale : ''}${to}`} >
+      {children}
     </Link>
   )
 }
