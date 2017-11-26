04cd20e4d3d0daa39269ed5493a14dc92fe5a0a6
Maurice Svay <maurice@svay.com>
2017-11-26 10:33:28 +0100

    Update TalkSubmission page style

--- components/TalkSubmissionContainer.js
+++ components/TalkSubmissionContainer.js
@@ -13,32 +13,32 @@ const TalkSubmissionContainer = () => (
       <meta name="description" content="Everything is awaysome!" />
     </Head>
 
-    <div className="container">
+    <div className="container talkSubmission">
       <h1>Proposer un sujet</h1>
-      <form>
-        <div>
-          <label>Titre</label>
-          <input type="text" name="title" required />
+      <form class="card talkSubmission__form">
+        <div className="formGroup">
+          <label for="talkSubmission-title">Titre</label>
+          <input type="text" name="title" required id="talkSubmission-title" />
         </div>
-        <div>
-          <label>Description</label>
-          <textarea name="extract" required></textarea>
+        <div className="formGroup">
+          <label for="talkSubmission-extract">Description</label>
+          <textarea name="extract" required rows="12" id="talkSubmission-extract"></textarea>
         </div>
-        <div>
+        <div className="formGroup">
           <label>Format</label>
           <label><input type="radio" name="format" value="long" checked /> Long (20mins)</label>
           <label><input type="radio" name="format" value="short" /> Court (5mins)</label>
         </div>
-        <div>
-          <label>Slides</label>
-          <input type="url" name="slides" />
+        <div className="formGroup">
+          <label for="talkSubmission-slides">Slides</label>
+          <input type="url" name="slides" placeholder="https://" id="talkSubmission-slides" />
         </div>
-        <div>
-          <label>Lien</label>
-          <input type="url" name="project" />
+        <div className="formGroup">
+          <label for="talkSubmission-project">Lien</label>
+          <input type="url" name="project" placeholder="https://" id="talkSubmission-project" />
         </div>
-        <div>
-          <input type="submit" value="Envoyer" />
+        <div className="formGroup">
+          <input type="submit" value="Envoyer" className="btn" />
         </div>
       </form>
     </div>
