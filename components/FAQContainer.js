952a5129366fe0ea95b8c5fbb119bb27807cd663
Maurice Svay <maurice@svay.com>
2017-11-26 10:37:27 +0100

    Update FAQ page style

--- components/FAQContainer.js
+++ components/FAQContainer.js
@@ -19,9 +19,11 @@ const FAQ = ({ hasError, isLoading, faq }) => (
             content={ textRenderer(faq.node.body).slice(0, 150) + "…" }
           />
         </Head>
-        <div className="container">
+        <div className="container FAQContainer">
           <h1>{ faq.node.title }</h1>
-          <BodyRenderer>{ faq.node.body }</BodyRenderer>
+          <div class="card">
+            <BodyRenderer>{ faq.node.body }</BodyRenderer>
+          </div>
         </div>
       </article>
     ) }
