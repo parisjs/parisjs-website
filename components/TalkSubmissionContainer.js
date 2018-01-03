87fc4e0618c5589c973bf481a24b46855cacd322
Fazouane Marouane <fazouanem3@gmail.com>
2018-01-03 20:04:19 +0100

    make form submissions great again

--- components/TalkSubmissionContainer.js
+++ components/TalkSubmissionContainer.js
@@ -1,19 +1,26 @@
-import React from 'react'
-import Head from 'react-helmet'
-import { createContainer, query, BodyRenderer } from '@phenomic/preset-react-app/lib/client'
-import { Link } from 'react-router'
-import hello from 'hellojs'
-import GitHub from 'github-api'
-import Form from 'react-jsonschema-form'
+import React from "react";
+import Head from "react-helmet";
+import {
+  createContainer,
+  query,
+  BodyRenderer
+} from "@phenomic/preset-react-app/lib/client";
+import { Link } from "react-router";
+import hello from "hellojs";
+import GitHub from "github-api";
+import Form from "react-jsonschema-form";
 
-import Layout from './Layout'
-import MeetupPreview from './MeetupPreview'
+import Layout from "./Layout";
+import MeetupPreview from "./MeetupPreview";
 
-hello.init({
-  github: 'Iv1.c13e7b19e913ffeb'
-}, {
-  redirect_uri: 'http://127.0.0.1:3333/propositions/sujet'
-});
+hello.init(
+  {
+    github: "Iv1.c13e7b19e913ffeb"
+  },
+  {
+    redirect_uri: "http://127.0.0.1:3333/propositions/sujet"
+  }
+);
 
 const schema = {
   type: "object",
@@ -48,13 +55,13 @@ const schema = {
       title: "Twitter"
     }
   }
-}
+};
 
 const uiSchema = {
   description: {
     "ui:widget": "textarea",
     "ui:options": {
-      "rows": 5
+      rows: 5
     }
   },
   kind: {
@@ -69,74 +76,74 @@ const uiSchema = {
   twitter: {
     "ui:placeholder": "@parisjs"
   }
-}
+};
 
 function getHelloGithubCred() {
-  const helloCreds = localStorage.getItem('hello');
+  const helloCreds = localStorage.getItem("hello");
 
   if (helloCreds) {
-    const parsedData = JSON.stringify(helloCreds)
+    const parsedData = JSON.stringify(helloCreds);
 
-    return (parsedData.github && parsedData.github.access_token) ? parsedData.github.access_token : null
+    return parsedData.github && parsedData.github.access_token
+      ? parsedData.github.access_token
+      : null;
   } else {
-    return null
+    return null;
   }
 }
 
 class TalkSubmissionContainer extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       githubToken: getHelloGithubCred(),
-      talk: {
-
-      }
-    }
+      talk: {}
+    };
     this.onSubmit = this.onSubmit.bind(this);
   }
 
   componentDidMount() {
-    hello.on('auth.login', () => {
-      const auth = hello('github').getAuthResponse();
+    hello.on("auth.login", () => {
+      const auth = hello("github").getAuthResponse();
 
       this.setState({
         githubToken: auth.access_token
-      })
+      });
     });
   }
 
-  getSubmitButtonLabel () {
-    return (this.state.githubToken) ? 'Soumettre' : 'Login avec github';
+  getSubmitButtonLabel() {
+    return this.state.githubToken ? "Soumettre" : "Login avec github";
   }
 
-  onSubmit({ form }) {
-    console.log(form)
-
-    console.log(this)
-
-    if (!this.state.githubToken) {
-      hello('github').login()
+  onSubmit({ formData }) {
+    const { githubToken } = this.state;
+    if (!githubToken) {
+      hello("github").login();
     } else {
       const gh = new GitHub({ token: githubToken });
 
-      console.log(gh.getUser())
+      //console.log(gh.getUser());
 
-      const remoteIssues = gh.getIssues('PierrickP', 'fluffy-octo-broccoli');
+      const remoteIssues = gh.getIssues("PierrickP", "fluffy-octo-broccoli");
 
-      // remoteIssues.createIssue({
-      //    title: form.dataFrom,
-      //    body: `
-      // *${title}*
-      //
-      // *Description :*
-      // > ${description}
-      //
-      // *Slides :* ${slideLink}
-      // *Projet :* ${projectLink}
-      //
-      // **
-      // `.trim(),
-      // });
+      console.log("formData", formData);
+      const { title, description, slideLink, projectLink } = formData;
+
+      remoteIssues.createIssue({
+        title: title,
+        body: `
+      *${title}*
+      
+      *Description :*
+      > ${description}
+      
+      *Slides :* ${slideLink}
+      *Projet :* ${projectLink}
+      
+      **
+      `.trim()
+      });
     }
   }
 
@@ -151,21 +158,30 @@ class TalkSubmissionContainer extends React.Component {
         <div className="container talkSubmission">
           <h1>Proposer un sujet</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, corporis illo sequi qui velit molestiae, in dicta eos totam magnam delectus, possimus temporibus suscipit non ratione laudantium ullam doloremque eius!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+            corporis illo sequi qui velit molestiae, in dicta eos totam magnam
+            delectus, possimus temporibus suscipit non ratione laudantium ullam
+            doloremque eius!
           </p>
-          <Form className="card talkSubmission__form"
-            schema={ schema }
-            uiSchema={ uiSchema }
-            formData={ this.state.talk }
-            onSubmit={ this.onSubmit } >
+          <Form
+            className="card talkSubmission__form"
+            schema={schema}
+            uiSchema={uiSchema}
+            formData={this.state.talk}
+            onSubmit={this.onSubmit}
+          >
             <div className="formGroup">
-              <input type="submit" value={ this.getSubmitButtonLabel() } className="btn" />
+              <input
+                type="submit"
+                value={this.getSubmitButtonLabel()}
+                className="btn"
+              />
             </div>
           </Form>
         </div>
       </Layout>
-    )
+    );
   }
 }
 
-export default TalkSubmissionContainer
+export default TalkSubmissionContainer;
