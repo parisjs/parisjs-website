51f78df8375f0ac6574f7ddf93573bf1e49d1128
freddy <freddy03h@gmail.com>
2018-01-13 19:27:54 +0100

    style next meetup

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import MdPerson from 'react-icons/lib/md/person'
 
 const Avatar = ({ imageUrl, size }) => (
   <div className={`Avatar ${size === 'small' && 'Avatar--small'}`}>
-    {imageUrl && <img src={imageUrl} className="Avatar__image" />}
+    {imageUrl ? (
+      <img src={imageUrl} className="Avatar__image" />
+    ) : (
+      <MdPerson color="#8E9599" size={18} className="Avatar__icon" />
+    )}
   </div>
 )
 
